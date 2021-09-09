01358b72516df78f7c7bf1dc791797afedfeb890
venkatesh <venkatesh12rr1a0308@gmail.com>
2021-09-09 07:58:54 +0000

    test cases passed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.json());
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
 let db = null;
@@ -137,3 +137,95 @@ app.post("/districts/", authenticateWithToken, async (request, response) => {
   await db.run(createNewDistrictQuery);
   response.send("District Successfully Added");
 });
+
+//get district based on district_id API 5
+
+app.get(
+  "/districts/:districtId/",
+  authenticateWithToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const getDistrictBasedOnIdQuery = `
+    SELECT
+       district_id AS districtId,
+       district_name AS districtName, 
+       state_id AS stateId,
+       cases, cured, active, deaths
+    FROM district
+    WHERE
+       district_id = ${districtId};`;
+    const districtObj = await db.get(getDistrictBasedOnIdQuery);
+    response.send(districtObj);
+  }
+);
+
+//delete a district API 6
+
+app.delete(
+  "/districts/:districtId/",
+  authenticateWithToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const deleteDistrictQuery = `
+    DELETE FROM district
+    WHERE
+       district_id = ${districtId};`;
+    await db.run(deleteDistrictQuery);
+    response.send("District Removed");
+  }
+);
+
+//update the district details API 7
+
+app.put(
+  "/districts/:districtId/",
+  authenticateWithToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const {
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = request.body;
+    const updateDistrictQuery = `
+    UPDATE district
+    SET
+      district_name = '${districtName}',
+      state_id = ${stateId},
+      cases = ${cases},
+      cured = ${cured},
+      active = ${active},
+      deaths = ${deaths}
+    WHERE
+      district_id = ${districtId};`;
+    await db.run(updateDistrictQuery);
+    response.send("District Details Updated");
+  }
+);
+
+//get statistics of covid cases API 8
+
+app.get(
+  "/states/:stateId/stats/",
+  authenticateWithToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+    const getStatisticsQuery = `
+    SELECT
+      SUM(cases) AS totalCases,
+      SUM(cured) AS totalCured,
+      SUM(active) AS totalActive,
+      SUM(deaths) AS totalDeaths
+    FROM
+      district
+    WHERE
+      state_id = ${stateId};`;
+    const covidStats = await db.get(getStatisticsQuery);
+    response.send(covidStats);
+  }
+);
+
+module.exports = app;
